37d682f27badd04aaace29ee96767ca2debf677c
cmis <cmis@cmis-hs18.(none)>
2015-08-14 14:35:56 +0700

    Created Hello

--- HelloUserAge/HelloUserAge/HelloUserAge.java
+++ HelloUserAge/HelloUserAge/HelloUserAge.java
@@ -1,2 +1,17 @@
 import java.util.Scanner;
 public class HelloUserAge
+    {
+         public static void main ( String [] args )
+        { Scanner stdin = new Scanner ( System.in );
+            String usersName;
+            
+            System.out.print("Please enter your name: ");
+            usersName = stdin.nextLine();
+            
+            String Age;
+            System.out.print("Please enter your age: ");
+            Age = stdin.nextLine();
+            
+            System.out.println("Hello, " + usersName + "! You are" + Age + "years old.");
+        }
+    }
