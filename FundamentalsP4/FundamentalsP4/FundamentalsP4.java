ddf9ecf8d52e053a4ff848d38fc7b6ddd0847ef0
cmis <cmis@cmis-hs17.(none)>
2015-08-24 09:56:38 +0700

    Not done

---
+++ FundamentalsP4/FundamentalsP4/FundamentalsP4.java
@@ -0,0 +1,8 @@
+public class FundamentalsP4
+    {public static void main ( String [] args )
+        {for( int ctr = 1; ctr < 11; ctr ++ )
+            {
+                System.out.println( ctr + "\t" + ctr*ctr + "\t" + (((int)Math.random()+ctr*ctr)));
+            }
+        }
+    }
