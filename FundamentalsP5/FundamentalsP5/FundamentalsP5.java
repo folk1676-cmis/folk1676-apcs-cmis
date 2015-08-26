0e71f12d70bafb7de201ca01123999375a9d086b
cmis <cmis@cmis-hs18.(none)>
2015-08-26 14:35:42 +0700

    not done

---
+++ FundamentalsP5/FundamentalsP5/FundamentalsP5.java
@@ -0,0 +1,11 @@
+public class FundamentalsP5
+    {public static void main ( String [] args )
+        {
+            String movie1 = "Southpaw";
+            String movie2 = "Fightclub";
+            String movie3 = "Million Dollar Baby";
+            String song1 = "Suicidal Thoughts";
+            String song2 = "Welcome to Forever";
+            String song3 = "Ghetto Gospel";
+        }
+    }
