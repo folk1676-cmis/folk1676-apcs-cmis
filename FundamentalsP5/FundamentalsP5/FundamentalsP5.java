e76a2fd82282950478212f39c38fa9b847e4929e
folk1676-cmis <folk1676@cmis.ac.th>
2015-08-30 23:44:05 +0700

    5 and 6

--- FundamentalsP5/FundamentalsP5/FundamentalsP5.java
+++ FundamentalsP5/FundamentalsP5/FundamentalsP5.java
@@ -2,8 +2,20 @@ public class FundamentalsP5
     {public static void main ( String [] args )
         {
             String[] favMov = { "Southpaw" , "Fight Club" , "Million Dollar Baby"};
-            for ( int index = 0; index < favMov.length; index++ )
-            {System.out.print( favMov[index]);
+            for ( int x = 0; x < favMov.length; x++ )
+            {
+                System.out.println( favMov[x]);
+            }
+        
+            System.out.println("\n" );
+            
+            String[] favSong = { "Welcome to Forever" , "Go Big or Go Home", "See You Again"  };
+            int x = 0;
+            while (x < favSong.length-1)
+            
+            {
+                System.out.print(favSong[x] + "," );
+                x++;
         }
     }
 }
