d3169fc76c6d1c7666e3407aa8761126e3e63476
cmis <cmis@cmis-hs21.(none)>
2015-09-03 10:51:04 +0700

    8

--- FundamentalsP8/FundamentalsP8/FundamentalsP8.java
+++ FundamentalsP8/FundamentalsP8/FundamentalsP8.java
@@ -1,42 +1,21 @@
 public class FundamentalsP8
 {
     public static void FundamentalsP8( String [] args )
-        {
-            String[] favArtists = { "Coldplay" , "Charlie Puth" , "Taylor Swift" };
-            for ( int x = 0; x < favArtists.length; x++ )
+       {
+           String[] favArtists = { "Coldplay" , "Charlie Puth" , "Taylor Swift" };
+           for ( int x = 0; x < favArtists.length; x++ )
             {
                 System.out.println( favArtists[x] );
             }
             
-            System.out.println("\n");
-            int x = (int)Math.random()*3;
-            favArtists[x] = "Michael Jackson";
+           System.out.println("\n");
             
-            if ( favArtists[x].equals( favArtists[0]))
+           favArtists[((int)Math.random()*3)] = "Michael Jackson";
+           for ( int x = 0; x < favArtists.length; x++ )
             {
-                System.out.println( "We found Michael Jackson!" );
+                System.out.println( favArtists[x] );
             }
-            else
-            {
-                System.out.println( "Michael Jackson is not in " + favArtists[0] );
-            }
-            
-            if ( favArtists[x].equals( favArtists[0]))
-            {
-                System.out.println( "We found Michael Jackson!" );
-            }
-            else
-            {
-                System.out.println( "Michael Jackson is not in " + favArtists[0] );
-            }
-            
-            if ( favArtists[x].equals( favArtists[0]))
-            {
-                System.out.println( "We found Michael Jackson!" );
-            }
-            else
-            {
-                System.out.println( "Michael Jackson is not in " + favArtists[0] );
-            }
-        }
+           
+           for ( int x = 0; x < favArtists.length; x++ )
     }
+}
