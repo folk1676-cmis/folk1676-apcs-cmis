e57e2f28408c3990d7480083805a495de54bf9ab
cmis <cmis@cmis-hs17.(none)>
2015-09-04 11:45:28 +0700

    .

--- Test1/Test1/Test1.java
+++ Test1/Test1/Test1.java
@@ -4,12 +4,25 @@ public class Test1
     public static void main ( String [] args )
     {
         String num = JOptionPane.showInputDialog("Enter an integer: "); 
-        int integer = Integer.parseInt(num);
+        int x = Integer.parseInt(num);
+        int []y = new int [x];
         
-        int num1 = integer.length;
-        for ( int index = 0; index < num.length; index++)
+        for ( int index = 0; index < y.length; index++ )
         {
-            System.out.println( num[index] );
+            System.out.println((double)(Math.random()*50));
         }
+        
+        int z = Integer.parseInt(num);
+        int r = x % 2;
+        
+        if (r == 0)
+        
+            {
+                System.out.println("E" + y);
+            }
+        else
+            {
+                System.out.println("O" + y);
+            }
     }
 }
