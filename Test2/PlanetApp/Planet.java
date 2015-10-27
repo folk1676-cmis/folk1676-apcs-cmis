74abc425ebb023d1c34a3a4762a82e6d7279c122
folk1676-cmis <folk1676@cmis.ac.th>
2015-10-28 00:18:35 +0700

    Update Planet.java

--- Test2/PlanetApp/Planet.java
+++ Test2/PlanetApp/Planet.java
@@ -4,13 +4,17 @@ public class Planet
     private double percentageSpaceMarines;
     private int maxSpaceMarinesPerStarDestroyer;
     private int nPlanets;
-    private Planet p;
+    private double radius;
+    private String p;
+    private String relationship;
     public Planet()
     {
         maxPopulation = 1;
         percentageSpaceMarines = 1;
         maxSpaceMarinesPerStarDestroyer = 1;
         nPlanets = 1;
+        radius = 1;
+        relationship = new String [] {"Hostile" , "At War", "Neutra", "Frenidly", "Ally"};
     }
     public Planet (int maxPopulation, double percentageSpaceMarines, int maxSpaceMarinesPerStarDestroyer, int nPlanets)
     {
@@ -22,7 +26,7 @@ public class Planet
     public String toString()
     {
         String output = new String();
-        output = "Name: " + "\n" + "Relationship: " + "\n" + "Radius: " +  "\n" + "Population: " + maxPopulation + "\n" + "Space Marines as percent of Pop: " +  "\n" + "Space Marines: " + percentageSpaceMarines +"\n" + "Star Destroyers: " + maxSpaceMarinesPerStarDestroyer +"\n" + "Space Marines per star destroyer: " + "" +"\n";
+        output = "Name: " + "\n" + "Relationship: " + relationship + "\n" + "Radius: " + radius +  "\n" + "Population: " + maxPopulation-((int)(Math.random())*maxPopulation) + "\n" + "Space Marines as percent of Pop: " + percentageSpaceMarines + "\n" + "Space Marines: " + maxPopulation-((int)(Math.random())*maxPopulation) +"\n" + "Star Destroyers: " + maxSpaceMarinesPerStarDestroyer +"\n" + "Space Marines per star destroyer: " + "" +"\n";
         return output;
     }
 }
