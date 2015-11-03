a2a71f0094da9f0abef5b37ec68e8510a62eb6f3
cmis <cmis@cmis-hs17.(none)>
2015-11-03 09:55:20 +0700

    doing 6

---
+++ ClassDesignP5/CardDriver2.java
@@ -0,0 +1,16 @@
+public class CardDriver2
+{
+    public static void main ( String[] args )
+    {
+        String [] Dealer = new String[2];
+        String [] P1 = new String [2];
+        String [] P2 = new String [2];
+        String [] P3 = new String [2];
+        String [] P4 = new String [2];
+        String [] P5 = new String [2];       
+        String [] Cards = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String [] Suits = {"♠", "♥", "♦", "♣"};
+        
+        
+    }
+}
