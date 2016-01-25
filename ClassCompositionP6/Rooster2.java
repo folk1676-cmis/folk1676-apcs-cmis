69ca14a6318d2bd16f55582e6a93bc188a11f90f
cmis <cmis@cmis-hs16.(none)>
2016-01-25 14:10:54 +0700

    selfie

---
+++ ClassCompositionP6/Rooster2.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class Rooster2
+{
+    ArrayList<Student> myStudents = new ArrayList<Student>();
+    public Rooster2()
+    {
+    }
+}
---
+++ ClassCompositionP6/RoosterDriver2.java
@@ -0,0 +1,14 @@
+public class RoosterDriver2
+{
+    private int x;
+
+    public RoosterDriver2()
+    {
+        x = 0;
+    }
+
+    public int sampleMethod(int y)
+    {
+        return x + y;
+    }
+}
