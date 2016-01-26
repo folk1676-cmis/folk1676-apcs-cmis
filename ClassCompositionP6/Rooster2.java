5752dec671b6cedcc6df800fadaa45c0a0e3275a
cmis <cmis@cmis-hs17.(none)>
2016-01-26 11:43:55 +0700

    6ix

--- ClassCompositionP6/Rooster2.java
+++ ClassCompositionP6/Rooster2.java
@@ -4,5 +4,34 @@ public class Rooster2
     ArrayList<Student> myStudents = new ArrayList<Student>();
     public Rooster2()
     {
+        Student x = new Student( "Min", "Cho", 10, 1.7, 1.0, 2.7, 2.3, 1.0 );
+        myStudents.add( x );
+    }//end
+
+    public void addStudent( Student newStudent )
+    {
+        myStudents.add( newStudent );
+    }//end
+
+    public void dropStudent( String lastname )
+    {
+        for ( int shark = 0; shark < myStudents.size(); shark++ )
+        {
+            if ( myStudents.get( shark ).equals( lastname ) );
+            {
+                myStudents.remove( shark );
+                shark--;
+            }
+        }
+    }//end
+    
+    public String toString ( )
+    {
+        String x = "";
+        for( Student y : myStudents )
+        {
+            x += y + "\n";
+        }
+        String output = String.format(
     }
 }
