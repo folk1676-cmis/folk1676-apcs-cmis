15cf5516d261e8ca5f87316b141271b44ebbf88a
cmis <cmis@cmis-hs17.(none)>
2016-02-02 14:29:02 +0700

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Array2D2/Odoamne.java
+++ Array2D2/Odoamne.java
@@ -32,14 +32,36 @@ public class Odoamne
             }
             System.out.println();
         }
+        
+        System.out.println();
+        
+        for( int row = 0; row < odoamne.length; row++ )
+        {
+            int rowSum = 0;
+            for( int col = 0; col < odoamne[0].length; col++ )
+            {
+                rowSum += odoamne[row][col];
+            }
+            System.out.println( "Sum of Row " + (row+1) + ": " + rowSum );
+        }
+
+        for( int col = 0; col < odoamne[0].length; col++ )
+        {
+            int colSum = 0;
+            for( int row = 0; row < odoamne.length; row++ )
+            {
+                colSum += odoamne[row][col];
+            }
+            System.out.println( "Sum of Column " + (col+1) + ": " + colSum );
+        }
 
         for( int[] row : odoamne )
         {
             for( int col : row )
             {
-                sum += odoamne.length;
-                System.out.print( sum );
+                sum += col;
             }
         }
+        System.out.println("Sum of Total: " + sum );
     }
 }
