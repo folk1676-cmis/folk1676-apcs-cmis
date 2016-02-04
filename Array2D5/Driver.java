870ed7d346833444ce0086f47cd971fb8027967e
folk1676-cmis <folk1676@cmis.ac.th>
2016-02-04 22:26:18 +0700

    donezo 5 lel

---
+++ Array2D5/Driver.java
@@ -0,0 +1,10 @@
+public class Driver
+{
+    public static void main ( String[] args )
+    {
+        JukeBox song = new JukeBox();
+        System.out.println( song );
+        System.out.println( "Song: " + song.randomSong());
+        System.out.println("Song(s) with 5 rating: " + song.playSongofRating(5));
+    }
+}
