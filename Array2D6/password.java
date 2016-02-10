81d179b1ce4dad2e648d533750d0907671dc234c
cmis <cmis@cmis-hs04.(none)>
2016-02-10 10:44:19 +0700

    i grew cancer cell

--- Array2D6/password.java
+++ Array2D6/password.java
@@ -3,6 +3,27 @@ public class password
 {
     public static void main ( String[] args )
     {
-        
+        String lname = JOptionPane.showInputDialog( "Lastname: " );
+        String pass = JOptionPane.showInputDialog( "Password: " );
+
+        int x = 0;     
+
+        String y = lname.toLowerCase();
+        String z = pass.toLowerCase();
+
+        while( x >= 0 )
+        {
+            x = z.indexOf( y );
+            if( x >= 0 )
+            {
+                System.out.println( "Please enter a valid password" );
+                z = JOptionPane.showInputDialog( "Lastname: " );
+                // System.out.println( "Please enter a valid password" );
+            }
+            else
+            {
+                System.out.println( "The password is valid." );
+            }
+        }
     }
 }
---
+++ ClassCompositionBigProblem/GPS.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+public class GPS
+{
+    ArrayList<Integer> markers = new ArrayList<Integer>();
+    public GPS()
+    {
+        markers.add( 100 );
+        markers.add( 150 );
+        markers.add( 105 );
+        markers.add( 120 );
+        markers.add( 90 );
+        markers.add( 80 );
+        markers.add( 50 );
+        markers.add( 75 );
+        markers.add( 75 );
+        markers.add( 70 );
+        markers.add( 80 );
+        markers.add( 90 );
+        markers.add( 100 );
+    }
+    
+    public GPS( ArrayList<Integer> markers )
+    {
+        
+    }
+}
