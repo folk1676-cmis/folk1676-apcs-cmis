bbfb6b9ec0e97a2974c0bd40974d691d0c3c98ca
cmis <cmis@cmis-hs04.(none)>
2016-02-10 10:56:15 +0700

    help

---
+++ ClassCompositionBigProblem/Trail.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+public class Trail
+{
+    ArrayList<Integer> markers = new ArrayList<Integer>();
+    int elevation;
+    public Trail()
+    {
+        markers.add( 100 );
+        markers.add( 150 );
+        markers.add( 105 );
+        markers.add( 120 );
+        markers.add( 90 );
+        markers.add( 80 );
+        markers.add( 50 );
+        markers.add( 75 );
+        markers.add( 75 );
+        markers.add( 70 );
+        markers.add( 80 );
+        markers.add( 90 );
+        markers.add( 100 );
+    }
+
+    public Trail(ArrayList<Integer> markers )
+    {
+        this.markers=markers;
+    } 
+
+    public void addMarker(int marker)
+    {
+        markers.add( marker );
+    }
+
+    public int getLength()
+
+    {
+        int sum=0;
+        for(Integer x: markers)
+        {  sum += x;
+        }
+        return sum;
+    }
+
+    public boolean isLevelTrailSegment( int begin, int end )
+    {
+        boolean check = false;
+        if (markers.get(begin)==markers.get(end))
+        {
+            check = true;
+            for (int i=begin;i<end;i++)
+            {
+                if (i > 0 && Math.abs(markers.get(i)-markers.get(i-1))>10)
+                {
+                    check=false;
+                }
+            }
+        }
+        if (check)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    public boolean isDifficult(int begin, int end)
+    {
+        elevation = 0;
+        for(int i=begin;i<end;i++)
+        {
+            if ( i > 0&& ( markers.get(i) - markers.get(i-1)) > 0 )
+            {
+                elevation += ( markers.get(i) -markers.get(i-1) );
+            }
+        }
+        if ( !isLevelTrailSegment(begin, end) && elevation > 100 )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public String toString()
+    {
+        String output = "Index\tElevation\n" ;
+        int i = 0;
+
+        for(Integer x: markers)
+        {  
+            i++;
+            output += i+"\t"+x+"\n";
+        }
+
+        output += "Total Distance of Trail is : "+getLength()+"\n";
+        output += "Trail Level: " +  isLevelTrailSegment( 0,markers.size()-1)+"\n";
+        output += "Trail Difficult: " +         isDifficult(0,markers.size()-1)+"\n";
+        output += "Net Elevation : " + elevation+"\n";
+        return output;
+    }
+}
