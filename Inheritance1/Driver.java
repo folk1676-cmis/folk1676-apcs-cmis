c2fbb6bcbd57b3af42cb2cf1c58efb500c637774
cmis <cmis@cmis-hs17.(none)>
2016-03-14 09:33:18 +0700

    hi

--- Inheritance1/Driver.java
+++ Inheritance1/Driver.java
@@ -1,7 +1,18 @@
 public class Driver
 {
-    public static void main ( String[] args )
+    public static void main(String[] args)
     {
-        
+        MyDevice[] Devices = new MyDevice[7];
+        Devices[0] = new MyDevice(32, "Blue");
+        Devices[1] = new MyPhone1(32, "Blue", true);
+        Devices[2] = new MyPhone2(32, "Blue", true, "LED");
+        Devices[3] = new MyPad(32, "Blue", true, "LED", 10.6);
+        Devices[4] = new MyPod(32, "Blue", 2000);
+        Devices[5] = new MyWatch(32, "Blue", "Digital");
+        Devices[6] = new MyTV(32, "Blue", false, "LED", 10.6, 1000);
+        for ( MyDevice device : Devices )
+        {
+            System.out.println(device + "\n");
+        }     
     }
 }
