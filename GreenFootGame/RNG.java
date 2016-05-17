6ebfca305156bba4cb0e9b4c30f7f87ffd485b59
cmis <cmis@cmis-hs15.(none)>
2016-05-17 11:45:17 +0700

    Nice

---
+++ GreenFootGame/RNG.java
@@ -0,0 +1,51 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RightAndLeft here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RNG extends Enemy
+{
+    public void act()
+    {      
+        movement();
+        turnAtWall();
+        die();
+    } 
+
+    public void movement()
+    {
+        move(4);
+        if(Greenfoot.getRandomNumber(50) <= 25)
+        {
+            turn(Greenfoot.getRandomNumber(180));
+        }
+        if(this.isAtEdge() == true)
+        {
+            turn(180);
+        }
+    }
+
+    public void turnAtWall()
+    {
+        if ( isTouching(Wall.class) )
+        {
+            turn(180);
+        }    
+    }
+
+    public void die()
+    {
+        Actor crab;
+        crab = getOneObjectAtOffset(0, 0, Player.class);
+        if(crab != null)
+        {
+            World world;
+            world = getWorld();
+            world.removeObject(crab);
+            Greenfoot.setWorld(new GameOver());
+        }
+    }
+}
