e67f49a2eb89e61c977b523eac152a575df0017a
folk1676-cmis <folk1676@cmis.ac.th>
2016-06-03 09:48:18 +0700

    Update JohnCena.java

--- GreenFootGame/JohnCena.java
+++ GreenFootGame/JohnCena.java
@@ -8,5 +8,39 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class JohnCena extends Player implements Character
 {
-
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("left"))
+        {
+            setLocation(getX()-4,getY());
+            if (isTouching(Wall.class))
+            {
+                setLocation(getX() + 4, getY());
+            }
+        }
+        if(Greenfoot.isKeyDown("right"))
+        {
+            setLocation(getX()+4,getY());
+            if (isTouching(Wall.class))
+            {
+                setLocation(getX() - 4, getY());
+            }
+        }
+        if(Greenfoot.isKeyDown("up"))
+        {
+            setLocation(getX(),getY()-4);
+            if (isTouching(Wall.class))
+            {
+                setLocation(getX(), getY() + 4);
+            }
+        }
+        if(Greenfoot.isKeyDown("down"))
+        {
+            setLocation(getX(),getY()+4);
+            if (isTouching(Wall.class))
+            {
+                setLocation(getX(), getY() - 4);
+            }
+        }
+    }
 }
