1dacecebf5fb52c61f22368c26039ea73d45441c
folk1676-cmis <folk1676@cmis.ac.th>
2016-06-03 09:49:05 +0700

    Update Player.java

--- GreenFootGame/Player.java
+++ GreenFootGame/Player.java
@@ -16,38 +16,6 @@ public abstract class Player extends Actor
     public void act() 
     {
         die();
-        if(Greenfoot.isKeyDown("left"))
-        {
-            setLocation(getX()-4,getY());
-            if (isTouching(Wall.class))
-            {
-                setLocation(getX() + 4, getY());
-            }
-        }
-        if(Greenfoot.isKeyDown("right"))
-        {
-            setLocation(getX()+4,getY());
-            if (isTouching(Wall.class))
-            {
-                setLocation(getX() - 4, getY());
-            }
-        }
-        if(Greenfoot.isKeyDown("up"))
-        {
-            setLocation(getX(),getY()-4);
-            if (isTouching(Wall.class))
-            {
-                setLocation(getX(), getY() + 4);
-            }
-        }
-        if(Greenfoot.isKeyDown("down"))
-        {
-            setLocation(getX(),getY()+4);
-            if (isTouching(Wall.class))
-            {
-                setLocation(getX(), getY() - 4);
-            }
-        }
     }
 
     public void die()
